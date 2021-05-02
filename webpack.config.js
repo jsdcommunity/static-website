acb3db063f81acbe7ee1844b73f921870bf9b409
Nizar K <nizar.dev01@gmail.com>
2021-05-02 16:27:16 +0530

    Configured webpack to bundle html templates

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,13 @@ const
     path = require('path'),
     HtmlWebpackPlugin = require('html-webpack-plugin'),
     HtmlWebpackInjector = require('html-webpack-injector'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
     { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    Handlebars = require('handlebars'),
+    
+    fs = require('fs'),
+    header = fs.readFileSync(__dirname + '/src/layout/header.html'),
+    footer = fs.readFileSync(__dirname + '/src/layout/footer.html'),
 
     isProd = process.env.NODE_ENV==='production',
 
@@ -14,10 +20,9 @@ const
         devtool: 'inline-source-map',
         devServer:{
             port: process.env.PORT || 3000,
-            contentBase: path.join(__dirname, '../dist'),
+            contentBase: path.join(__dirname, './dist'),
             overlay: true,
             open: true,
-            openPage: '/acceleration.html',
             hot: true,
             publicPath: '/'
         },
@@ -46,6 +51,53 @@ const
                             presets: ['@babel/preset-env']
                         }
                     }
+                },
+                {
+                    test: /\.(html)$/,
+                    use: [
+                        {
+                            loader: 'html-loader',
+                            options: {
+                                preprocessor: (content, loaderContext) => {
+                                    let result;
+                        
+                                    try {
+                                      result = Handlebars.compile(content)({
+                                        header,
+                                        footer
+                                      });
+                                    } catch (error) {
+                                      loaderContext.emitError(error);
+                        
+                                      return content;
+                                    }
+                        
+                                    return result;
+                                }
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'images/[hash].[ext]'
+                    }
+                },
+                {
+                    test: /\.styl$/,
+                    use: isProd ? [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        "css-loader",
+                        "stylus-loader"
+                    ] : [
+                        'style-loader',
+                        "css-loader",
+                        "stylus-loader"
+                    ]
                 }
             ]
         }
@@ -62,18 +114,25 @@ const
                     collapseWhitespace: false,
                     removeComments: false
                 } : null,
-                template: data.html,
+                template: `./src/${data.html.replace('.html','').replace('.htm','')}.html`,
                 filename: `${filename}.html`,
-                chunks: data.chunks,
+                chunks: data.chunks || [ filename ],
                 favicon: data.favicon
             })
         )
         // Adding the javascript file for the page
-        config.entry[filename] = data.js
+        if(data.js) config.entry[data.jsfile || filename] = `./src/js/${data.js.replace('.js','')}.js`
     }
 
 require('./webpack.entry.js').forEach(entry => pageFactory(entry))
 
-console.log(config)
+if(isProd){
+    config.plugins.push(
+        new MiniCssExtractPlugin({
+            filename:'css/[name].[hash].css',
+        }),
+        new CleanWebpackPlugin()
+    )
+}
 
 module.exports = config
--- webpack.entry.js
+++ webpack.entry.js
@@ -1,8 +1,10 @@
 module.exports = [
     {
-        html: "./src/index.html",
-        filename: "index",
-        js: "./src/scripts.js",
-        chunks: ['index']
-    }
+        html: "index.html", // filename and the chunk's name will be index
+        js: "scripts"
+    },
+    {
+        html: "home.html",
+        chunks: ['index'] // will load the chunk index instead of bundling its own js file
+    },
 ]
