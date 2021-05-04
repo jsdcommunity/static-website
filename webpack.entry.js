f5a8609ad7272ad99015ab08f0933d6dbd104513
Mohammed Musthafa <muhammed.mustafa@emvigotech.com>
2021-05-04 18:46:21 +0530

    home.html removed

--- webpack.entry.js
+++ webpack.entry.js
@@ -2,9 +2,5 @@ module.exports = [
     {
         html: "index.html", // filename and the chunk's name will be index
         js: "scripts"
-    },
-    {
-        html: "home.html",
-        chunks: ['index'] // will load the chunk index instead of bundling its own js file
-    },
+    }
 ]
